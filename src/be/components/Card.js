e0912e9c6bb10627b8c77c235ea7095c1f9120f3
ejpservice <ejptheservice@gmail.com>
2023-05-24 20:53:12 -0400

    Disenando el Panel

--- src/be/components/Card.js
+++ src/be/components/Card.js
@@ -2,14 +2,13 @@ import { Link } from "react-router-dom"
 
 import circlesImg from '../../images/circles.png'
 
-const Card = (props) => {
-   const {link, leftColor, rightColor, description, title, icon} = props
+const Card = ({ link, leftColor, rightColor, value, title, icon }) => {
 
-   return(
+   return (
       <Link to={link}>
-         <div className="total" style={{backgroundImage: `url(${circlesImg}), linear-gradient(to right, ${leftColor}, ${rightColor})`}}>
+         <div className="total" style={{ backgroundImage: `url(${circlesImg}), linear-gradient(to right, ${leftColor}, ${rightColor})` }}>
             <div className="titulo">
-               <h3>{description}</h3>
+               <h3>{value}</h3>
                <h3>{title}</h3>
             </div>
             <div className="icono" >
--- src/be/views/Panel.js
+++ src/be/views/Panel.js
@@ -1,101 +1,94 @@
 import { useEffect, useState } from "react"
 import axios from "axios";
-import { Link } from "react-router-dom";
 
 import { HeaderController } from "../components/HeaderController"
 import Card from "../components/Card";
 
 import '../../css/Card.css'
-
-import circlesImg from '../../images/circles.png'
+import { Activos } from "../components/Activos";
 
 export const Panel = () => {
    const url = 'http://localhost:9005/api/';
 
+   const [sales, setSales] = useState([]);
    const [customers, setCustomers] = useState([]);
+   const [inventory, setInventary] = useState([]);
+   const [teamMemberAsset, setTeamMemberAsset] = useState([]);
+   const [teamMemberInactive, setTeamMemberInactive] = useState([]);
 
    useEffect(() => {
       const panel = document.getElementById("Panel");
       panel.style.setProperty("transform", "translateY(0px)")
 
       getEventHeld();
-   })
+   }, [])
 
    const getEventHeld = async () => {
+      const sa = await axios(`${url}panelSales`);
+      setSales(sa.data[0].totalSales);
+
       const res = await axios(`${url}panelCustomers`);
-      setCustomers(res.data);
+      setCustomers(res.data[0].totalCustomer);
+
+      const inv = await axios(`${url}panelInventory`);
+      setInventary(inv.data[0].totalAmount);
+
+      const tma = await axios(`${url}panelTeamMemberAsset`);
+      setTeamMemberAsset(tma.data[0].totalTeamMember);
+
+      const tmi = await axios(`${url}panelTeamMemberInactive`);
+      setTeamMemberInactive(tmi.data[0].totalTeamMember);
    }
-   
-   const [date, setDate] = useState([{
-      "id": 2,
-      "link": "/be/views/inventory",
-      "leftColor": "#A74CF2",
-      "rightColor": "#c87121",
-      "description": "5",
-      "title": "Total Inventario",
-      "icon": "https://cdn-icons-png.flaticon.com/512/1291/1291749.png"
-   }, {
-      "id": 3,
-      "link": "/be/views/customers",
-      "leftColor": "#A74CF2",
-      "rightColor": "#255c77",
-      "description": "10",
-      "title": "Total Cliente",
-      "icon": "https://cdn-icons-png.flaticon.com/512/9752/9752345.png"
-   }, {
-      "id": 4,
-      "link": "/be/views/team_member",
-      "leftColor": "#A74CF2",
-      "rightColor": "#47ceff",
-      "description": "125",
-      "title": "Miembros De Equipo",
-      "icon": "https://cdn-icons-png.flaticon.com/512/609/609104.png"
-   }, {
-      "id": 5,
-      "link": "/be/views/team_member",
-      "leftColor": "#A74CF2",
-      "rightColor": "#279c50",
-      "description": "25",
-      "title": "ME Activos",
-      "icon": "https://cdn-icons-png.flaticon.com/512/706/706170.png"
-   }, {
-      "id": 6,
-      "link": "/be/views/team_member",
-      "leftColor": "#A74CF2",
-      "rightColor": "#e41616",
-      "description": "25",
-      "title": "ME Inactivos",
-      "icon": "https://cdn-icons-png.flaticon.com/512/692/692453.png"
-   }])
 
    return (
       <div className="container">
          <HeaderController />
 
          <div className="totales">
-            <Link to={'/be/views/sales'}>
-               <div className="total" style={{ backgroundImage: `url(${circlesImg}), linear-gradient(to right, #A74CF2, #617BFB)` }}>
-                  <div className="titulo">
-                     <h3>{/*customers[0].total*/}13</h3>
-                     <h3>Total Clientes</h3>
-                  </div>
-                  <div className="icono" >
-                     <img src="https://cdn-icons-png.flaticon.com/512/5567/5567444.png" alt="" />
-                  </div>
-               </div>
-            </Link>
+         <Card
+               link='be/views/sales'
+               leftColor='#A74CF2'
+               rightColor='#47ceff'
+               value={sales}
+               title='Total Sales'
+               icon='https://cdn-icons-png.flaticon.com/512/5567/5567444.png'
+            />
 
-            <Link to={'/be/views/inventory'}>
-               <div className="total" style={{ backgroundImage: `url(${circlesImg}), linear-gradient(to right, #A74CF2, #c87121)` }}>
-                  <div className="titulo">
-                     <h3>7</h3>
-                     <h3>Total Inventario</h3>
-                  </div>
-                  <div className="icono" >
-                     <img src="https://cdn-icons-png.flaticon.com/512/609/609104" alt="" />
-                  </div>
-               </div>
-            </Link>
+            <Card
+               link='be/views/customers'
+               leftColor='#A74CF2'
+               rightColor='#617BFB'
+               value={customers}
+               title='Total Clientes'
+               icon='https://cdn-icons-png.flaticon.com/512/9752/9752345.png'
+            />
+
+            <Card
+               link='be/views/inventory'
+               leftColor='#A74CF2'
+               rightColor='#c87121'
+               value={inventory}
+               title='Total Inventario'
+               icon='https://cdn-icons-png.flaticon.com/512/1291/1291749.png'
+            />
+
+            <Card
+               link='be/views/registerTeamMember'
+               leftColor='#A74CF2'
+               rightColor='#279c50'
+               value={teamMemberAsset}
+               title='ME Activo'
+               icon='https://cdn-icons-png.flaticon.com/512/609/609104.png'
+            />
+
+            <Card
+               link='be/views/registerTeamMember'
+               leftColor='#A74CF2'
+               rightColor='#e41616'
+               value={teamMemberInactive}
+               title='ME Inactivo'
+               icon='https://cdn-icons-png.flaticon.com/512/692/692453.png'
+            />
          </div>
       </div>
    )
