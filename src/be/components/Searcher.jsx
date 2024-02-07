e529f8045f95582e573c9a1500304dccb6409879
ejpservice <ejptheservice@gmail.com>
2024-02-07 19:47:16 -0400

    eliminando archivos

--- src/be/components/Searcher.jsx
+++
@@ -1,7 +0,0 @@
-export const Searcher = ({holder, setSearch}) => {
-   return (
-      <div>
-         <input type="text" name="campo" id="campo" onChange={(e) => setSearch(e.target.value)} placeholder={holder} />
-      </div>
-   )
-}
